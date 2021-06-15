b5b1ec59ebffb3ee2a5ba92e0117049c6d86d519
evirevialle <83653214+evirevialle@users.noreply.github.com>
2021-06-15 12:14:55 +0200

    Add files via upload

---
+++ Interface_cakephp_15.06/templates/Pages/publications.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Info Page Related Articles
+*/
+?>
+
+<div class="row">
+    <?= $this->element('sideNav', ['mapBox' => false, 'export' => 'simple'])?>
+    <div class="column-responsive column-80">
+		<div class="content">
+			<h3><?= __('Publikationen') ?></h3>
+			<p>Hier kann man Veröffentlichungen zum Projekt finden!</p>
+		</div>
+	</div>
+</div>
