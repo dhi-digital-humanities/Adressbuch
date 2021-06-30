e92de70fcd5a83bf1e35370446b60151929da1f6
evirevialle <83653214+evirevialle@users.noreply.github.com>
2021-06-30 11:34:43 +0200

    Add files via upload

--- adressbuch1854/templates/Streets/view.php
+++ adressbuch1854/templates/Streets/view.php
@@ -4,6 +4,10 @@
  * @var \App\Model\Entity\Street $street
  */
 	use Cake\Collection\Collection;
+
+
+	require(__DIR__.'/../functions/img_zotero.php');
+	require(__DIR__.'/../functions/varsstreets.php');
 	
 	$varieties = [];
 	foreach($sameStreets as $sameStr){
@@ -19,7 +23,6 @@
 			array_push($arrsNew, $this->Html->link($arr->no, ['controller' => 'Arrondissements', 'action' => 'view', $arr->id]));
 		}
 	}
-
 	
 ?>
 <div class="row">
@@ -73,29 +76,12 @@
            <div>
 <?php endif; ?>
 
-		<?php 		
-				   $street_name=$street->name_old_clean;
-			       $street=htmlentities($street);
-			      
-
- 			function citationzotero($street_name, $street){
-
-		    	$code_span= '<span class="Z3988" title="url_ver=Z39.88-2004&amp;ctx_ver=Z39.88-2004&amp;rfr_id=info%3Asid%2Fzotero.org%3A2&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook&amp;rft.genre=bookitem&amp;rft.atitle=';
-		        $code_span.=$street_name;
-		        $code_span.='&amp;rft.description=';
-		        $code_span.=$street;
-		        $code_span.='&amp;rft.btitle=Adressbuch%20der%20Deutschen%20in%20Paris%20f%C3%BCr%20das%20Jahr%201854&amp;rft.place=Paris&amp;rft.edition=Elektronische%20Edition&amp;rft.aufirst=F.&amp;rft.aulast=Kronauge&amp;rft.au=F.%20Kronauge&amp;rft.date=1854&amp;rft.language=Allemand">'; 
-		        
-
-        	return ($code_span);
-                                      } ?>
-
 </div>
 
  <br><div class="csl-bib-body" style="line-height: 1.35; margin-left: 2em; text-indent:-2em;">
   <div class="csl-entry">Kronauge, F. «&nbsp;<?php echo $street_name ?>&nbsp;». In <i>Adressbuch der Deutschen in Paris für das Jahr 1854</i>, Elektronische Edition, 1854. <a target="_blank" href='<?php  $this->request->getUri() ?>'><?php echo $this->request->getUri() ?></a>.</div>
 
-<?php print citationzotero($street_name, $street);?>
+<?php print zoterostreets($street_name, $street_new)?>
           
 
 </div>
