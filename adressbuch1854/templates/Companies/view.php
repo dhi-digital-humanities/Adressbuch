d0c15d6f6e9e28f320939f1b7a21defcd68b3f7c
evirevialle <83653214+evirevialle@users.noreply.github.com>
2021-06-30 11:31:53 +0200

    Add files via upload

--- adressbuch1854/templates/Companies/view.php
+++ adressbuch1854/templates/Companies/view.php
@@ -4,6 +4,9 @@
  * @var \App\Model\Entity\Company $company
  */
 
+require_once(__DIR__.'/../functions/img_zotero.php');
+require(__DIR__.'/../functions/varscomp.php');
+
  $this->Html->css('view');
 
 	$pageRefs = [];
@@ -31,33 +34,14 @@
 			<?=	!empty($pageRefs) ? __('Eintrag im Buch auf ').implode(' und ', $pageRefs).'.' : '' ?>
             <table>
             	<tr>
-            		<th><?= __('Numerisation de la page')?></th>
+            		<th><?= __('Scan der Seite')?></th>
             		<td>
-            			<?php
-
-					function afficherimage($path, $taille, $id, $begP)
-						{
-							$code_html_image = "<a target='_blank' href=/../img/HD/BHVP_703983_$begP.jpg>";
-							$code_html_image.=" <img src='";
-							$code_html_image.= $path;
-							$code_html_image.= $taille;
-							$code_html_image.= $id;
-							$code_html_image.= $begP;
-							$code_html_image.= ".jpg'";
-							$code_html_image.= " width = '100'";
-							$code_html_image.= "title= 'consulter l IHA pour exploiter la page $begP'";
-
-
-						return ($code_html_image); 
-						}
-
-							print afficherimage('/../img/','SD/','BHVP_703983_',$begP);?><br>
+							<?php print image('/../img/','SD/','BHVP_703983_',$begP);?><br>
 						
-
 						<details>
-							<summary>voir la page en HD</summary>
+							<summary>Seite in HD ansehen</summary>
 						<form>
-							<button type='submit' title="Consulter l'IHA pour exploiter la page <?php echo $begP?>" formtarget='_blank' formaction='/../img/HD/BHVP_703983_<?php echo $begP ?>.jpg'
+							<button type='submit' title="IHA zur Nutzung der Seite <?php echo $begP?>" formtarget='_blank' formaction='/../img/HD/BHVP_703983_<?php echo $begP ?>.jpg'
 
 							value="text">BHVP_703983_<?php echo $begP?>.jpg</button>
 						</form>
@@ -66,10 +50,10 @@
             	   </td> 
             	</tr>
             	<tr>
-            		<th><?= __('Ocerisations')?></th>
+            		<th><?= __('Volltexterkennung')?></th>
             		<td>
             		<details>
-							<summary>voir la page océrisée</summary>
+							<summary>Volltext der Seite ansehen</summary>
 						<form>
 							<button type='submit' formtarget='_blank' formaction='/../Ocerisations/BHVP_703983_<?php echo $begP ?>.txt'
 
@@ -137,31 +121,14 @@
 			</div>
 			<div>
 <?php endif; ?>
-			       <?php 
- 
-			       	$nachname=$company->name;
-			       	$company=htmlentities($company);
-
- function citationzotero($begP, $nachname, $company){
-
-    	$code_span= '<span class="Z3988" title="url_ver=Z39.88-2004&amp;ctx_ver=Z39.88-2004&amp;rfr_id=info%3Asid%2Fzotero.org%3A2&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook&amp;rft.genre=bookitem&amp;rft.atitle=';
-    	$code_span.=$nachname;
-    	$code_span.='&amp;rft.description=';
-    	$code_span.=$company;
-    	$code_span.='&amp;rft.btitle=Adressbuch%20der%20Deutschen%20in%20Paris%20f%C3%BCr%20das%20Jahr%201854&amp;rft.place=Paris&amp;rft.edition=Elektronische%20Edition&amp;rft.aufirst=F.&amp;rft.aulast=Kronauge&amp;rft.au=F.%20Kronauge&amp;rft.date=1854&amp;rft.pages=';
-        $code_span.=$begP;
-        $code_span.= '&amp;rft.language=Allemand">';
-        
-
-        return ($code_span);
-                                      } ?>
+			     
 
 </div>
 
  <br><div class="csl-bib-body" style="line-height: 1.35; margin-left: 2em; text-indent:-2em;">
   <div class="csl-entry">Kronauge, F.«&nbsp;<?php echo $nachname ?>&nbsp;». In <i>Adressbuch der Deutschen in Paris für das Jahr 1854</i>, Elektronische Edition., <?php echo $begP ?>, 1854. <a target="_blank" href='<?php  $this->request->getUri() ?>'><?php echo $this->request->getUri() ?></a>.</div>
 
-<?php print citationzotero($begP, $nachname, $company);?>
+<?php print zoterocomp($nachname, $prof_category, $specification, $profession, $begP);?>
 </div>
 
 </div>
