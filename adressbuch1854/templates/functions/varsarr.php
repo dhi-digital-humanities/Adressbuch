e0aafd4d5c0e7d883213009a21f03fc438c7763d
evirevialle <83653214+evirevialle@users.noreply.github.com>
2021-07-12 15:47:16 +0200

    Delete adressbuch1854 directory

--- adressbuch1854/templates/functions/varsarr.php
+++
@@ -1,8 +0,0 @@
-<?php 		
-				  
-//$arr1 is a variable to save insee's citycode on zotero with zoterocomp's function
-			       $arr1=$arrondissement['insee_citycode'];
-			      
-
- 			
-                                      
